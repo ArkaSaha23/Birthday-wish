a82bd7113497e10ca0ec9b01b8ed4a1d60117d80
Arka Saha <arkacomputer2004@gmail.com>
2026-04-27 23:57:06 +0530

    Notes.jsx

--- src/assets/components/Notes.jsx
+++ src/assets/components/Notes.jsx
@@ -4,8 +4,8 @@ function Notes() {
   return (
     <section className="birthday-card">
       <div className="birthday-card__header">
-        <span className="badge">Birthday Surprise</span>
-        <h1>Happy Birthday, MONAAA! 🎂</h1>
+        <span className="badge">Birthday Surprise🥰🤩</span>
+        <h1>Happy Birthday, MONAAA!🎂</h1>
         <p>
           Wishing you a day full of laughter, love, and bright memories. May
           every moment sparkle just like you.
@@ -15,30 +15,30 @@ function Notes() {
       <div className="birthday-card__details">
         <div className="birthday-card__detail">
           <p>
-            <span className="descrption">Bhaiii reee🎉🥳</span>
+            <span className="descrption">Bhaiii Reeeeeeeee!!!!🎉🥳❤️</span>
             <span className="descrption">
-              Aajker din ta completely tor jonno...enjoy it to the fullest.
+              Aajker din ta completely tor jonno...enjoy it to the fullest🍫🎂.
               Seriously bolchi, tui na thakle amr college life ekdom pocha hoto. Thank you for always being there, jotoi choto choto
               bepar hok. college jawa,college thekhe asha, adda dewa, random,antabri, odhbut topics nea gossips, sob ki6u moja lage korte.
             </span>
 
             <span className="descrption">
-              Thank you for being so kind and helpful...Sobai emon hoy na,
+              Thank you for being so kind and helpful😍...Sobai emon hoy na,
               ar eta tor biggest strength. Jokhon amr ki6u...dorkar hoy tui thakis.
             </span>
 
             <span className="descrption">
               Aar ekta kotha bolbo...life e jotoi ups and downs ashuk, tui jemon
               achis temon thak. Tui onek kichu achieve korbi, ami 100% sure.
-              Placement, career, future — sob jaygay tui bhalo korbi. Just nijer
+              Placement, career, future — sob jaygay tui bhalo korbi👍💪. Just nijer
               upor trust rakh, ar overthink kom kor(joto ta possible 😄).
             </span>
 
             <span className="descrption">
-              Aajke khub moja kor, bhalo kore gel...barite a6is...khawa dawa kor...treat ta nebo...taka ready rakhiss, btw onk din dhore bole6i jadavpur er oi Biriyani restaurant tay niye jete(jetar kotha tui bole6ilis)tui akhono niye jas ni,Lojjaa laga dorkar. jaihok,
+              Aajke khub moja kor, bhalo kore gel...barite a6is...khawa dawa kor...treat ta nebo...taka ready rakhiss, btw onk din dhore bole6i jadavpur er oi Biriyani restaurant tay niye jete(jetar kotha tui bole6ilis)tui akhono niye jas ni😤😒,Lojjaa laga dorkar😬. jaihok,
               </span>
-              <span>Once again, Happy
-              Birthday🤍
+              <span className="buttom-wish">Once again, Happy
+              Birthday❤️😍🥰🤩🥳🍫🍬🍭🍮🎂
             </span>
           </p>
         </div>
